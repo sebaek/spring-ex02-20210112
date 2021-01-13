249840a5699a9b644f6a00488f16e0ab765c17d2
sebaek <sebaek@gmail.com>
2021-01-13 16:23:09 +0900

    test

--- src/test/java/org/zerock/mapper/CommentMapperTests.java
+++ src/test/java/org/zerock/mapper/CommentMapperTests.java
@@ -1,5 +1,6 @@
 package org.zerock.mapper;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -33,5 +34,62 @@ public class CommentMapperTests {
 		List<CommentVO> list = mapper.getList();
 		assertNotEquals(list.size(), 0);
 	}
+	
+	@Test
+	public void testInsert() {
+		CommentVO comment = new CommentVO();
+		comment.setBno(1L);
+		comment.setContent("new comment");
+		comment.setWriter("newbie");
+		
+		int before = mapper.getList().size();
+		
+		mapper.insert(comment);
+		
+		int after = mapper.getList().size();
+		
+		assertEquals(before + 1, after);
+	}
+	
+	/*
+	@Test
+	public void testInsertSelectKey() {
+		CommentVO comment = new CommentVO();
+		comment.setBno(1L);
+		comment.setContent("new comment");
+		comment.setWriter("newbie");
+		
+		int before = mapper.getList().size();
+		
+		mapper.insertSelectKey(comment);
+		
+		int after = mapper.getList().size();
+		
+		assertEquals(before + 1, after);
+	}
+	
+	@Test
+	public void testRead() {
+		CommentVO comment = new CommentVO();
+		comment.setBno(1L);
+		comment.setContent("new comment");
+		comment.setWriter("newbie");
+		
+		mapper.insertSelectKey(comment);
+		
+		CommentVO com = mapper.read(comment.getCno());
+		assertNotNull(com);
+		assertNotEquals(com.getCno(), new Long(0));
+	}
+	*/
 
 }
+
+
+
+
+
+
+
+
+
