4392d28a131a963cc52ae9341929b5367c2c1181
sebaek <sebaek@gmail.com>
2021-01-13 16:08:13 +0900

    comment

--- src/test/java/org/zerock/mapper/CommentMapperTests.java
+++ src/test/java/org/zerock/mapper/CommentMapperTests.java
@@ -1,5 +1,6 @@
 package org.zerock.mapper;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.CommentVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
