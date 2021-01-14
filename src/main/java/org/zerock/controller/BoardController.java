eec8d1662b5ff14dfa3a349d12af4bdc519e4476
sebaek <sebaek@gmail.com>
2021-01-14 14:27:22 +0900

    pic

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -1,20 +1,33 @@
 package org.zerock.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("/board/*")
 public class BoardController {
+	
+	private BoardService service;
+	
+	/*
+	public BoardController(BoardService service) {
+		super();
+		this.service = service;
+	}
+	*/
 
 	// 211 page 표
 //	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	@GetMapping("/list")
 	public void list() {
-		
+		service.getList();
 	}
 	
 //	@RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -24,16 +37,17 @@ public class BoardController {
 	}
 	
 	// 표: /board/read, 코드: /board/get
+	@GetMapping("/get")
 	public void get() {
 		
 	}
 	
-	
+	@PostMapping("/modify")
 	public String modify() {
 		return null;
 	}
 	
-	
+	@PostMapping("/remove")
 	public String remove() {
 		return null;
 	}
