3485d3ca6a43f31e775ec7eec592a5e18cee540a
sebaek <sebaek@gmail.com>
2021-01-25 15:38:14 +0900

    json

--- src/main/java/org/zerock/controller/RestControllerEx2.java
+++ src/main/java/org/zerock/controller/RestControllerEx2.java
@@ -2,13 +2,54 @@ package org.zerock.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Rest1;
+
+import lombok.extern.log4j.Log4j;
 
 @RestController
 @RequestMapping("/rest2")
+@Log4j
 public class RestControllerEx2 {
 
 	@RequestMapping("/ex1")
 	public String method1() {
 		return "hello";
 	}
+
+	@RequestMapping("/ex2")
+	public Rest1 method2() {
+		
+		// 전송, 수신 방법이 http(HyperText Transper Protocol)
+		log.info("method2");
+		
+		Rest1 r = new Rest1();
+		r.setName("donald");
+		r.setAge(33);
+		
+		return r;
+	}
+	
+	@RequestMapping("/ex3")
+	public String method3() {
+		
+		// 전송, 수신 방법이 http(HyperText Transper Protocol)
+		log.info("method3");
+		
+		Rest1 r = new Rest1();
+		r.setName("donald");
+		r.setAge(33);
+		
+//		String res = "이름:" + r.getName() + ", " + "나이:" + r.getAge();
+		String res = "{\"name\":\"" + r.getName() + "\", \"age\":" + r.getAge() + "}";
+		return res;
+	}
 }
+
+
+
+
+
+
+
+
+
