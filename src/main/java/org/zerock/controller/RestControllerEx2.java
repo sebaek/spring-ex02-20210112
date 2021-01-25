72b518adf452b28fe74fdea1498a6dbb3d080d73
sebaek <sebaek@gmail.com>
2021-01-25 16:27:39 +0900

    json

--- src/main/java/org/zerock/controller/RestControllerEx2.java
+++ src/main/java/org/zerock/controller/RestControllerEx2.java
@@ -3,6 +3,7 @@ package org.zerock.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.Rest1;
+import org.zerock.domain.Rest2;
 
 import lombok.extern.log4j.Log4j;
 
@@ -55,6 +56,21 @@ public class RestControllerEx2 {
 		r.setAge(33);
 		
 		return r;
+	}
+	
+	@RequestMapping("/ex5")
+	public Rest2 method5() {
+		Rest2 r2 = new Rest2();
+		r2.setAddress("seoul");
+		
+		Rest1 r1 = new Rest1();
+		r1.setName("jeju");
+		r1.setAge(100);
+		r1.setVote(true);
+		
+		r2.setRest1(r1);
+		
+		return r2;
 	}
 }
 
