cb5a62cef7389bb4d8a678af1186b2190b9baee5
sebaek <sebaek@gmail.com>
2021-01-26 14:19:28 +0900

    mediatype"

--- src/main/java/org/zerock/controller/RestControllerEx3.java
+++ src/main/java/org/zerock/controller/RestControllerEx3.java
@@ -1,5 +1,6 @@
 package org.zerock.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,6 +63,13 @@ public class RestControllerEx3 {
 		log.info(body);
 		
 		return "method7";
+	}
+	
+	@RequestMapping(path = "/ex8", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String method8(@RequestBody String body) {
+		log.info(body);
+		
+		return "method8";
 	}
 }
 
