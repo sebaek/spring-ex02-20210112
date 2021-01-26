59fdb55bb69ce5a9b241e0e8a4d66b2bc302b9d3
sebaek <sebaek@gmail.com>
2021-01-26 11:26:29 +0900

    path variable

--- src/main/java/org/zerock/controller/RestControllerEx3.java
+++ src/main/java/org/zerock/controller/RestControllerEx3.java
@@ -1,6 +1,8 @@
 package org.zerock.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.log4j.Log4j;
@@ -15,6 +17,14 @@ public class RestControllerEx3 {
 		log.info("name:" + name);
 		
 		return "spring";
+	}
+	
+	@RequestMapping("/ex2/{val}") // path variable
+	public String method2(@PathVariable("val") String value) {
+		log.info("method2");
+		log.info(value);
+		
+		return "method2";
 	}
 }
 
