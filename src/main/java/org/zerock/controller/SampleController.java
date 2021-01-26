e74aeeb15abef7b78d7f15afbcb995d02d227155
sebaek <sebaek@gmail.com>
2021-01-26 17:54:03 +0900

    sample

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,9 +1,12 @@
 package org.zerock.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
 import org.zerock.domain.Ticket;
 
 import lombok.extern.log4j.Log4j;
@@ -12,15 +15,22 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @RequestMapping("/sample")
 public class SampleController {
-	
+
 	@PostMapping("/ticket")
 	public Ticket conver(@RequestBody Ticket ticket) {
 		log.info(ticket);
-		
+
 		return ticket;
 	}
+
+	@GetMapping(value = "/getSample", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
+	public SampleVO getSample() {
+		SampleVO s = new SampleVO();
+		s.setFirstName("donald");
+		
+		return s;
+	}
+	
+	
 }
-
-
-
-
