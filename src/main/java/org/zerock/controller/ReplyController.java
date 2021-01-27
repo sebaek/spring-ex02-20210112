e7cc3baec8b5e265514e7e8d061b69391c36d517
sebaek <sebaek@gmail.com>
2021-01-27 11:11:33 +0900

    delete

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,19 @@ public class ReplyController {
 		ReplyVO vo = service.get(rno);
 		
 		return new ResponseEntity<ReplyVO>(vo, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{rno}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> remove(
+			@PathVariable("rno") Long rno) {
+		
+		int cnt = service.remove(rno);
+		
+		if (cnt == 1) {
+			return new ResponseEntity<>("success", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
 
