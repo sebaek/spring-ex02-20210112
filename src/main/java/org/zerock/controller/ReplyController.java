bb17d75583a94ce3538e0e7e233eecd232d3df34
sebaek <sebaek@gmail.com>
2021-01-27 10:15:51 +0900

    get

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -1,12 +1,17 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
@@ -37,6 +42,19 @@ public class ReplyController {
 		} else {
 			return new ResponseEntity<> (HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+	
+	@GetMapping(value = "/pages/{bno}/{page}",
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable("page") int page,
+			@PathVariable("bno") Long bno) {
+		
+		Criteria cri = new Criteria(page, 10);
+		
+		List<ReplyVO> list = service.getList(cri, bno);
+		
+		return new ResponseEntity<List<ReplyVO>> (list, HttpStatus.OK);
 	}
 }
 
