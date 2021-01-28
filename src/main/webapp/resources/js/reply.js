72075728e85412cdc477ca17fa4b0bc7ffc4d081
sebaek <sebaek@gmail.com>
2021-01-28 16:28:04 +0900

    update

--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -59,11 +59,31 @@ var replyService = (function() {
 		});
 	}
 
+	function update(reply, callback, error) {
+		$.ajax({
+			type: 'put',
+			url: '/controller/replies/' + reply.rno,
+			data: JSON.stringify(reply),
+			contentType: 'application/json; charset=utf-8',
+			success: function(result, status, xhr) {
+				if (callback) {
+					callback(result);
+				}
+			},
+			error: function(xhr, status, er) {
+				if (error) {
+					error(er);
+				}
+			}
+		});
+	}
+
 	return {
 //		name:"AAAA",
 		add: add,
 		getList: getList,
-		remove: remove	
+		remove: remove,
+		update: update
 	};
 })();
 
